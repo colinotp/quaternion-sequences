299ae5d1327aec0c8498a155f872320c9e057d23
colinotp <paul.colinot@outlook.com>
2023-07-28 12:28:37 -0400

    modified sorting script

--- rust/sortpairs.sh
+++ rust/sortpairs.sh
@@ -1,29 +1,13 @@
+#! /bin/sh
 
+type=$1
+n=$2
 
-n=$1
-
-for filename in "result/pairs/*.pair"
+for dirname in results/pairs/$type/find_$n/*;
 do
-	if [ -n "$2" ] && [ $2 -ne $c ] && [ $2 -ne -1 ]
-	then
-		continue
-	fi
-	if [ -e matchings/$n.$c.$l.AB.pafs.txt ]
-	then
-		START=$(date +%s.%N)
-		sort -T tmp/ matchings/$n.$c.$l.AB.pafs.txt > matchings/$n.$c.$l.AB.pafs.sorted.txt
-		END=$(date +%s.%N)
-		DIFF=$(echo "$END - $START" | bc)
-		printf "  Case $c: AB PAFs sorted in %.2f seconds\n" $DIFF
-		echo $DIFF > timings/$n.$c.$l.AB.sorttime
-	fi
-	if [ -e matchings/$n.$c.$l.CD.pafs.txt ]
-	then
-		START=$(date +%s.%N)
-		sort -T tmp/ matchings/$n.$c.$l.CD.pafs.txt > matchings/$n.$c.$l.CD.pafs.sorted.txt
-		END=$(date +%s.%N)
-		DIFF=$(echo "$END - $START" | bc)
-		printf "  Case $c: CD PAFs sorted in %.2f seconds\n" $DIFF
-		echo $DIFF > timings/$n.$c.$l.CD.sorttime
-	fi
+	for filename in $dirname/*.pair;
+	do
+		echo $filename
+		sort -g $filename > $filename.sorted
+	done
 done
