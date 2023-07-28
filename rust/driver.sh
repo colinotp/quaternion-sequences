a8dad8c0db0256e874b1b6669ad0a4dc7d001d6f
colinotp <paul.colinot@outlook.com>
2023-07-28 18:08:27 -0400

    Followup of the two previous cmmits

--- rust/driver.sh
+++ rust/driver.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+# This driver computes the perfect Williamson Type sequences for the given n:
+# ./driver.sh n
+#
+# At the moment, it doesn't return the list of sequences, but just the number of sequences.
+# I'll add it quickly.
+
 
 if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 then
@@ -12,9 +18,23 @@ n=$1
 
 start=`date +%s`
 
+# Creating every necessary file
+start2=`date +%s`
 cargo run pairs $n
+end2=`date +%s`
+echo Sorting the files took `expr $end2 - $start2` seconds.
+
+# sorting the files
+start2=`date +%s`
 ./sortpairs.sh wts $n
+end2=`date +%s`
+echo Sorting the files took `expr $end2 - $start2` seconds.
+
+# Matching the file AND reducing to equivalence
+start2=`date +%s`
 cargo run join $n
+end2=`date +%s`
+echo Sorting the files took `expr $end2 - $start2` seconds.
 
 end=`date +%s`
 echo Execution time was `expr $end - $start` seconds.
