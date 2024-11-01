cd26450fc3a01abf93eb6ebc38540d79010e81ae
benne-aidan <bennettaidan03@gmail.com>
2024-11-01 09:46:25 -0400

    Started writing script to split sort Committing to test on DRAC servers

---
+++ rust/sortpairs_split.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+if [ ! -e tmp/ ]
+then
+	mkdir tmp/
+fi
+
+type=$1
+n=$2
+
+
+export LC_ALL=C
+
+for dirname in results/pairs/$type/find_$n/*;
+do
+	if [ -d $dirname ]
+	then
+		for filename in $dirname/*.pair;
+		do
+			echo $filename
+			for filename in "$dirname"/*.pair;
+			do
+				base_name=$(base_name "$filename" .pair)
+
+				output_dir="$dirname/$base_name"
+				mkdir -p "$output_dir"
+
+				split -d -l 15000000 "$filename" "$output_dir/${base_name}_part_"
+			done
+		done
+	fi
+done
