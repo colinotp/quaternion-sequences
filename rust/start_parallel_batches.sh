611c9005f797ca2130c9de7536f0f8b46b865c6b
benne-aidan <bennettaidan03@gmail.com>
2025-06-03 18:28:12 -0400

    Expanded parallelization script to run full algorithm

--- rust/start_parallel_batches.sh
+++ rust/start_parallel_batches.sh
@@ -28,15 +28,46 @@ for d in "$foldername"/rowsum_*; do
   fi
 done
 
+# Generate rowsums
 ./target/release/rust rowsums $n 
 
 
+jobids=()
 
-# read the rowsums file
+# read the rowsums file and submit jobs
 input="results/pairs/wts/find_$n/rowsums.quad"
 while IFS= read -r rowsum
 do
-    #launch the batches for each rowsum
-    sbatch ./job_pair_single_rowsum.sh $n $rowsum $rowsum_pairing 1
-    sbatch ./job_pair_single_rowsum.sh $n $rowsum $rowsum_pairing 2
+    # Submit job for first pair, capturing job ID
+	jobid=$(sbatch ./job_pair_single_rowsum.sh $n $rowsum $rowsum_pairing 1 | awk '{print $4}')
+	# Check for successful job submission
+	if [[ -z "$jobid" ]]; then
+		echo "Failed to submit job for rowsum $rowsum"
+		exit 1
+	fi
+	jobids+=($jobid)
+	echo "Submitted job $jobid for rowsum $rowsum"
+
+	# Submit job for first pair, capturing job ID
+	jobid=$(sbatch ./job_pair_single_rowsum.sh $n $rowsum $rowsum_pairing 2 | awk '{print $4}')
+	# Check for successful job submission
+	if [[ -z "$jobid" ]]; then
+		echo "Failed to submit job for rowsum $rowsum"
+		exit 1
+	fi
+	jobids+=($jobid)
+	echo "Submitted job $jobid for rowsum $rowsum"
+
 done < "$input"
+
+dep_string=$(IFS=:; echo "${jobids[*]}")
+jobid=$(sbatch --dependency=afterok:$dep_string ./job_sort.sh wts $n | awk '{print $4}')
+# Check for successful job submission
+if [[ -z "$jobid" ]]; then
+	echo "Failed to submit sort job"
+	exit 1
+fi
+echo "Submitted job $jobid for sorting"
+
+sbatch --dependency=afterok:$jobid ./job_join.sh $n
+
