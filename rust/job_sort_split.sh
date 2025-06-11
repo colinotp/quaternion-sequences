209e2569393a96f83638f33c6523d0131a8e1bed
benne-aidan <bennettaidan03@gmail.com>
2025-06-10 21:18:30 -0400

    Finished alternative sorting algorithm Hybrid merge/bash sort for use with excessively large file sizes

---
+++ rust/job_sort_split.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#SBATCH --time=30:00:00
+#SBATCH --mem=4000M
+#SBATCH --account=def-cbright
+
+# Script to run a SLURM job on a DRAC cluster
+#
+# This script sorts the .pair files generated in the first part of the algorithm,
+# using a combination of merge sort and bash quick sort for handling of large files.
+
+# Example usage: sbatch job_sort_split.sh n
+# where n is the desired length
+
+
+n=$1
+./sortpairs_split.sh $n
--- rust/sortpairs_split.sh
+++ rust/sortpairs_split.sh
@@ -1,35 +1,46 @@
-#! /bin/sh
+#! /bin/bash
 
-# WIP
-# Intended to split up the files being sorted into smaller files, and sort them individually to ease filesystem load
+if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+then
+	echo "This driver sorts the .pair files generated for a given length n in the first part of the algorithm, using a combination of merge sort and bash quick sort for handling of large files."
+	echo "Example usage: ./sortpairs_split.sh n"
+	exit 0
+fi
 
 if [ ! -e tmp/ ]
 then
 	mkdir tmp/
 fi
 
-type=$1
-n=$2
+n=$1
 
 
 export LC_ALL=C
 
-for dirname in results/pairs/$type/find_$n/*;
+for dirname in results/pairs/wts/find_$n/*;
 do
 	if [ -d $dirname ]
 	then
 		for filename in $dirname/*.pair;
 		do
+			# Create directories
 			echo $filename
-			for filename in "$dirname"/*.pair;
+			base_name=$(basename "$filename" .pair)
+			output_dir="$dirname/$base_name"
+			mkdir -p "$output_dir"
+
+			# Split up files into smaller files and sort
+			split -d -l 1000000 "$filename" "$output_dir/${base_name}_part_"
+			for file in $output_dir/${base_name}_part_*;
 			do
-				base_name=$(base_name "$filename" .pair)
-
-				output_dir="$dirname/$base_name"
-				mkdir -p "$output_dir"
-
-				split -d -l 15000000 "$filename" "$output_dir/${base_name}_part_"
+				# Use this command for sorting on compute canada
+				sort -S 1G -T $SLURM_TMPDIR $filename > $filename.sorted
+				# Use this command for any sorting done not using SLURM
+				# sort -S 1G -T tmp/ $filename > $filename.sorted
 			done
+			# Merge sorted files together
+			sort -m $output_dir/*.sorted > $filename.sorted
 		done
+
 	fi
 done
