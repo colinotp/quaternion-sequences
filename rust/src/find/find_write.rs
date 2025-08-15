8c863c6f7ea830eb606e3d84a84fb1eb230ab1af
Aidan Bennett <bennettaidan03@gmail.com>
2025-08-14 20:02:36 -0400

    Added preprocessing to filter sequences before fully reducing to eqiuvalence

--- rust/src/find/find_write.rs
+++ rust/src/find/find_write.rs
@@ -2,7 +2,7 @@ use std::{f64, fs::{self, DirEntry, File}, io::{Error, Write}, time::Instant};
 use itertools::iproduct;
 use memory_stats::memory_stats;
 
-use crate::{find::find_unique::reduce_to_equivalence, read_lines, sequences::{fourier::iter_over_enumerate_filtered_couples_psds, matching::{compute_auto_correlation_pair_dft, compute_cross_psd_pair}, rowsum::{generate_rowsums, generate_sequences_with_rowsum, rowsum, sequence_to_string, Quad}, symmetries::*, williamson::{QuadSeq, SequenceTag}}, str_to_seqtype};
+use crate::{find::find_unique::{reduce_to_equivalence_par}, read_lines, sequences::{equivalence::{equivalent_automorphism, equivalent_even_alternated_negation, equivalent_reverse, equivalent_uniform_shift}, fourier::iter_over_enumerate_filtered_couples_psds, matching::{compute_auto_correlation_pair_dft, compute_cross_psd_pair}, rowsum::{generate_rowsums, generate_sequences_with_rowsum, rowsum, sequence_to_string, Quad}, symmetries::*, williamson::{QuadSeq, SequenceTag}}, str_to_seqtype};
 
 
 
@@ -441,8 +441,12 @@ pub fn join_pairs(p : usize, seqtype : SequenceType) -> Vec<QuadSeq>{
 
     println!("\ncount before equivalences {}", result.len());
     let time = Instant::now();
-    let reduced = reduce_to_equivalence(&result, seqtype, &seqtype.equivalences());
-    let elapsed = time.elapsed().as_secs_f32().round() as isize;
+    let filtered = match seqtype {
+        SequenceType::QuaternionType => {reduce_to_equivalence_par(&result, seqtype, &vec![equivalent_even_alternated_negation, equivalent_reverse, equivalent_uniform_shift, equivalent_automorphism])},
+        _ => {result}
+    };
+    let reduced = reduce_to_equivalence_par(&filtered, seqtype, &seqtype.equivalences());
+    let elapsed = time.elapsed().as_secs();
 
     println!("count after equivalences {}", reduced.len());
     println!("Reducing to equivalence took: {} seconds.", elapsed);
