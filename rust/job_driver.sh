e4e82b244d9bf9aeeda9fe45a02c979f75d22f63
Curtis Bright <cbright@gmail.com>
2025-10-30 12:05:52 -0400

    Update driver call in example Slurm job script

--- rust/job_driver.sh
+++ rust/job_driver.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --mem=2000M
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=4096M
 #SBATCH --account=def-cbright
 
 # Script to run a SLURM job on a DRAC cluster
 #
 # Simply calls the main driver script to compute QTS/PQS of a given length
 
-./driver.sh 17
+./driver.sh qts 17 -dhs
