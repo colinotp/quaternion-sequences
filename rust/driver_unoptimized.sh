0c1e3e0e8d520456387b499b523b22182181333d
Aidan Bennett <bennettaidan03@gmail.com>
2025-11-10 15:39:55 -0500

    Updated unoptimized driver script

--- rust/driver_unoptimized.sh
+++ rust/driver_unoptimized.sh
@@ -1,35 +1,46 @@
 #! /bin/bash
 
 # This driver computes sequences for the given length n:
-# ./driver_unoptimized.sh qts n
+# ./driver.sh qts n
 #
 # Optional flags:
 # -d: delete existing .seq, .pair and .sorted files
-# -p <pairing>: run code with chosen pairing of sequences. Options include XY, XW, XZ
+# -p <pairing>: run code with chosen pairing of sequences. Options include WX, WY and WZ
 
 
 if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 then
 	echo "This driver computes sequences for the given length n, with additional error checking and debugging turned on:"
-	echo "./driver.sh <sequencetype> <n>"
+	echo "./driver_unoptimized.sh <sequencetype> <n> [flags]"
 	echo "Optional flags:"
 	echo "  * -d: Delete existing .seq, .pair and .sorted files"
 	echo "  * -c: Use auto/cross correlation for matching instead of PSD/CPSD"
 	echo "  * -p <pairing>: Specify rowsum pairing to be used. Options include XY, XZ, XW. Default is XW"
+	echo "  * -h: Convert sequences to Hadamard matrices when finished"
 	exit 0
 fi
 
 type=$1
 n=$2
+
+if [ -z "$type" ] || [ -z "$n" ]; then
+	echo 'Incorrect args passed. Try running with --help.'
+	exit 1
+fi
+
 shift
 shift
 foldername="./results/pairs/$type/find_$n"
-rowsum_pairing="XW"
+rowsum_pairing="WZ"
+hadamard=false
 match_option="psd"
 
 # Empty out existing .pair files to avoid conflicts
-while getopts "cdp:" flag; do
+while getopts "chdp:" flag; do
 	case $flag in
+		h)
+		hadamard=true
+		;;
 		d)
 		./pair_file_cleanup.sh $type $n
 		;;
@@ -61,44 +72,49 @@ then
 	mkdir -p $foldername
 fi
 
-start=`date +%s`
+start=`date +%s.%N`
 
 # Creating every necessary file
-start2=`date +%s`
-cargo run pairs $type $n $match_option $rowsum_pairing &> $filename
+cargo run pairs $type $n $match_option $rowsum_pairing | tee $filename
 if [ $? -ne 0 ]
 then
-	echo 'ERROR: pairs exited unsuccessfully. See log for details'
+	echo 'ERROR: pairs exited unsuccessfully. See log for additional details'
 	exit 1
 fi
-end2=`date +%s`
-echo Creating the sequences took `expr $end2 - $start2` seconds. 
-echo -e Creating the sequences took `expr $end2 - $start2` seconds. "\n \n" >> $filename
 
-# sorting the files
-start2=`date +%s`
-./sortpairs.sh $type $n &>> $filename
+# Sort pairs
+./sortpairs.sh $type $n
 if [ $? -ne 0 ]
 then
-	echo 'ERROR: sort exited unsuccessfully. See log for details'
+	echo 'ERROR: sort exited unsuccessfully. See log for additional details'
 	exit 1
 fi
-end2=`date +%s`
-echo Sorting the files took `expr $end2 - $start2` seconds.
-echo -e Sorting the files took `expr $end2 - $start2` seconds. "\n \n" >> $filename
 
 # Matching the file AND reducing to equivalence
-start2=`date +%s`
-cargo run join $type $n &>> $filename
+cargo run join $type $n | tee $filename -a
 if [ $? -ne 0 ]
 then
-	echo 'ERROR: join exited unsuccessfully. See log for details'
+	echo 'ERROR: join exited unsuccessfully. See log for additional details'
 	exit 1
 fi
-end2=`date +%s`
-echo Matching the sequences took `expr $end2 - $start2` seconds.
-echo -e Matching the sequences took `expr $end2 - $start2` seconds. "\n \n" >> $filename
 
-end=`date +%s`
-echo Total execution time was `expr $end - $start` seconds.
-echo -e Total execution time was `expr $end - $start` seconds. >> $filename
+if [ $hadamard = true ]; then
+	start2=`date +%s.%N`
+	cargo run convert hm $type $n | tee $filename -a
+	end2=`date +%s.%N`
+	elapsed=$(echo "$end2 - $start2" | bc)
+	printf "Converting to matrices up to Hadamard equivalence took %.2f seconds\n" $elapsed | tee $filename -a
+	
+	filename2="$foldername/result.mat"
+	matcount=$(wc -l < $filename2)
+	echo "$matcount matrices were found after converting up to Hadamard equivalence." | tee $filename -a
+fi
+
+echo "Converting PQS to QHM ..." | tee $filename -a
+./convert_qhm.sh $type $n
+
+end=`date +%s.%N`
+elapsed=$(echo "$end - $start" | bc)
+printf "\nTotal execution time was %.2f seconds.\n" $elapsed | tee $filename -a
+
+echo "This output can also be found in $filename"
