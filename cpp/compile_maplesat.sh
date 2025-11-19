f36c2558fdadbb32432f261064fa6a5e972bd715
Curtis Bright <cbright@gmail.com>
2025-11-19 16:55:37 -0500

    Remove unused directories and update readme

--- cpp/compile_maplesat.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d maplesat/maplesat ]
-then
-	git clone https://bitbucket.org/JLiangWaterloo/maplesat.git
-fi
-cd maplesat/maplesat
-git checkout qw
-make maplesat
-cp simp/maplesat_static ../..
