1408221dfa3b979153c61898a1393243eb183ee3
Curtis Bright <cbright@gmail.com>
2026-01-30 04:12:11 -0500

    Remove test_dft; in test_dft_inv test the DFT and inverse DFT on a sequence of length 10

--- rust/src/tests/test_fourier.rs
+++ rust/src/tests/test_fourier.rs
@@ -1,12 +1,11 @@
 
 #[cfg(test)]
 mod tests {
-    use num_complex::Complex64;
+    //use num_complex::Complex64;
 
     use crate::sequences::{fourier::{dft_sequence, inverse_dft}, rowsum::generate_sequences_with_rowsum};
 
-
-    #[test]
+    /*#[test]
     fn test_dft(){
         let eps = 0.01;
         
@@ -28,11 +27,11 @@ mod tests {
 
         let seq = vec![1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
         let _ = dft_sequence(&seq);
-    }
+    }*/
 
     #[test]
     fn test_dft_inv() {
-        let seq : Vec<i8> = vec![-1,1,-1,-1];
+        let seq : Vec<i8> = vec![1,-1,-1,-1,1,1,-1,1,-1,1];
 
         let dft = dft_sequence(&seq);
         let inv : Vec<i8> = inverse_dft(&dft, seq.len()).iter().map(|elm| elm.round() as i8).collect();
